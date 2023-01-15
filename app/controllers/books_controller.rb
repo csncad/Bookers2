0b9867e35c9f557f842e3b52cee7bac60db74606
EC2 Default User <117186071+csncad@users.noreply.github.com>
2023-01-15 17:09:32 +0000

    bookers2[adding]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,34 @@
 class BooksController < ApplicationController
   def new
-    @post_image = book
+    @book = Book.new
+  end
+  
+  def index
+    @books= Book.all
+    @book=Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  
+  devise_for :users
+  root to: 'homes#top'
+  resource :books
+  post 'books' => 'books#create'
+  get 'books/show'
+  get 'books/index'
   get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users/show'
   get 'users/edit'
-  devise_for :users
-  root to: 'homes#top'
+  get 'homes/about' => 'about#new'
 end
